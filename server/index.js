c1475c0a7306dcc9b9cd088ec8f8e8fe76e830ba
Andrew Knox <aknox0329@gmail.com>
2017-12-04 16:39:00 -0800

    removing all mongoose and mongodb references

--- server/index.js
+++ server/index.js
@@ -1,39 +1,16 @@
 const express = require('express')
 const bodyParse = require('body-parser')
-const bcrypt = require('bcrypt')
-const mongoose = require('mongoose')
-const user = require('./models/user')
-
-mongoose.Promise = Promise
-
-const promise = mongoose.connect('mongodb://<username>:<password>@ds127936.mlab.com:27936/bears-08', {
-    useMongoClient: true
-})
-promise.then(function db() {
-    console.log('Connected to the database.')
-})
+const hash = require('./policies/hash')
+const register = require('./controllers/registercontroller')
 
 const app = express()
 
 app.use(bodyParse.urlencoded({ extended: false }))
 app.use(bodyParse.json())
 
-app.post('/register', function register(req, res) {
-    const saltRounds = 10
-    const plainPass = req.body.password
-    bcrypt.genSalt(saltRounds, function salter(err, salt) {
-        bcrypt.hash(plainPass, salt, function hash(err, hash) {
-            req.body.password = hash
-
-            let newUser = user(req.body)
-            newUser.save(function save(err, newUser) {
-                if (err) return console.error(err)
-                res.send('You have been registered successfully ' + newUser.firstname)
-            })
-        })
-    })
+app.post('/register', function bcrypt(req, res) {
+    req.body.password = hash(10, req.body.password)
+    if (register(req.body)) res.send('')
 })
-
-
 
 app.listen(3000, 'localhost')
