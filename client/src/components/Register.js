4bb6a2cfa55d07ccf279b8edeb4cd207be12e5f9
Andrew Knox <aknox0329@gmail.com>
2017-12-13 20:18:43 -0800

    sorry had to commit to show someone the repo

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import AuthenticationService from '../services/AuthenticationService';
-
+import '../styles/register.css';
 
 class Register extends Component {
     constructor(props) {
@@ -13,10 +13,12 @@ class Register extends Component {
             username: '',
             email: '',
             password: '',
-            confirmpassword: ''
+            confirmpassword: '',
+            match: false
         };
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.passMatch = this.passMatch.bind(this);
     }
 
     handleSubmit(event) {
@@ -28,7 +30,19 @@ class Register extends Component {
         const name = target.name;
         this.setState({
             [name]: target.value
-        })
+        });
+    }
+
+    passMatch(password, passConf) {
+        if (password !== passConf) {
+            this.setState({
+                match: false
+            });
+        } else {
+            this.setState({
+                match: true
+            });
+        }
     }
 
     render() {
@@ -39,26 +53,42 @@ class Register extends Component {
                     name="firstname"
                     value={this.state.firstname}
                     onChange={this.handleChange}
+                    type="text"
                 />
                 <TextField
                     floatingLabelText="last name"
                     name="lastname"
+                    value={this.state.lastname}
+                    onChange={this.handleChange}
+                    type="text"
                 />
                 <TextField
                     floatingLabelText="username"
                     name="username"
+                    value={this.state.username}
+                    onChange={this.handleChange}
+                    type="text"
                 />
                 <TextField
                     floatingLabelText="email"
                     name="email"
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                    type="email"
                 />
                 <TextField
                     floatingLabelText="password"
                     name="password"
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                    type="password"
                 />
                 <TextField
                     floatingLabelText="confirm password"
                     name="confirmpassword"
+                    value={this.state.confirmpassword}
+                    onChange={this.handleChange}
+                    type="password"
                 />
                 <RaisedButton 
                     onClick={this.handleSubmit}
